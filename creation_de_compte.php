7a0df4162ad7ba59f4bdfc441071d0b4440216f9
Andy00006 <andy.laloum8@gmail.com>
2026-04-03 19:24:51 +0200

    Update and rename creation_de_compte.html to creation_de_compte.php

--- creation_de_compte.php
+++ creation_de_compte.php
@@ -1,3 +1,59 @@
+<?php
+$erreur = "";
+$classe_erreur = "";
+
+if(isset($_POST["prenom"])){
+    $mdp = $_POST["mdp1"];
+    $confirmation = $_POST["mdp2"];
+
+    if($mdp !== $confirmation){
+        $erreur = "Les mots de passe ne sont pas identiques.";
+        $classe_erreur = "input-erreur";
+    } 
+    else{
+        $majuscule = false;
+        $chiffre = false;
+        $special = false;
+
+        $lettres = str_split($mdp);
+
+        foreach($lettres as $key){
+            if(ctype_upper($key)) $majuscule = true;
+            if(ctype_digit($key)) $chiffre = true;
+            if(!ctype_alnum($key)) $special = true; 
+        }
+
+        if(strlen($mdp) < 12 || !$majuscule || !$chiffre || !$special){
+            $erreur = "Il faut une majuscule, un chiffre, un caractère spécial et 12 caractères minimum.";
+            $classe_erreur = "input-erreur";
+        }
+    }
+    if($erreur == "" && isset($_POST["mdp1"])){
+        $fichier = "utilisateurs.json";
+                    
+         $contenu = file_get_contents($fichier);
+         $utilisateurs = json_decode($contenu, true);
+
+         $nouveau = array(
+            "prenom" => $_POST["prenom"],
+            "nom" => $_POST["nom"],
+            "email" => $_POST["email"],
+            "date" => $_POST["anniversaire"],
+            "mdp" => $_POST["mdp1"],
+            "role" => "client"
+        );
+
+        $utilisateurs[] = $nouveau;
+                    
+        $json_final = json_encode($utilisateurs);
+        file_put_contents($fichier, $json_final);
+
+        header("Location: connexion_au_compte.php");
+        exit();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +66,7 @@
 <body>
 
 <div class="inscription">
-    <form action="#" method="post">
+    <form action="creation_de_compte.php" method="post">
         
         <div class="entete">
             <a href="accueil.html">
@@ -86,13 +142,17 @@
             <div class="groupe-ligne">
                 <div class="saisie">
                     <label for="mdp1">Mot de passe</label>
-                    <input type="password" id="mdp1" name="mdp1" placeholder="Mot de passe" required>
+                    <input type="password" id="mdp1" name="mdp1" class="<?php echo $classe_erreur; ?>" placeholder="Mot de passe" required>
                 </div>
                 <div class="saisie">
                     <label for="mdp2">Confirmation</label>
-                    <input type="password" id="mdp2" name="mdp2" placeholder="Confirmer" required>
+                    <input type="password" id="mdp2" name="mdp2" class="<?php echo $classe_erreur; ?>" placeholder="Confirmer" required >
                 </div>
             </div>
+            <?php if($erreur !== ""){
+                echo "<p style='color: #ff4d4d; font-size: 14px; margin-bottom: 10px; margin-left: 5px;'> $erreur </p>  ";     
+            }
+            ?>
         </div>
 
         <div class="consentement">
