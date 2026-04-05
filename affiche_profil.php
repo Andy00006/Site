87d4c72e6ce93e95dc1c4c2f63161fe42f4bf7e9
Andy00006 <andy.laloum8@gmail.com>
2026-04-05 19:19:25 +0200

    Update affiche_profil.php

--- affiche_profil.php
+++ affiche_profil.php
@@ -30,7 +30,7 @@ $initiales = mb_strtoupper($initiale_p . $initiale_n);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Profil</title>
+    <title>Profil</title>
     <link rel="stylesheet" href="profil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="couleur.css">
