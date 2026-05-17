b2e348af99f883aabf0c3aac9cade55e5af042c4
paulo4895 <vinzantpaularthur@gmail.com>
2026-05-17 22:09:24 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -129,6 +129,7 @@ if (file_exists($histo_file)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Profil</title>
+    <script src="darkmode.js"></script>
     <link rel="stylesheet" href="profil.css?v=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="couleur.css">
